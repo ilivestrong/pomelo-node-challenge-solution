08e9fbd0d28e7152b92794dc366600e49dcdf28a
Deepak <deepak@central.tech>
2020-08-18 23:31:26 +0700

    put some documentation

--- src/server.js
+++ src/server.js
@@ -1,13 +1,19 @@
 const Hapi = require("@hapi/hapi");
 
+// override port number and host name via config file.
 const { PORT, HOST } = require("./server.config");
 
+/**
+ * define routes and configure Hapi server
+ * @returns void
+ */
 const init = async () => {
   const server = Hapi.Server({
     port: PORT,
     host: HOST,
   });
 
+  /* route to format json */
   server.route({
     method: 'POST',
     path: '/format',
@@ -17,13 +23,17 @@ const init = async () => {
     }
   });
 
+  /* kick off the server */
   await server.start();
+
   console.log(`Server running at http://${HOST}:${PORT}`);
 }
 
+/* tap the unhandledRejection event, log the error and let Node exit abnormally. */
 process.on('unhandledRejection', (err) => {
   console.log(err);
   process.exit(1);
 });
 
+// start the server
 init();
