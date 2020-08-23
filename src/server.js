46a630333a2abe9e767f54e135f3d20d38f15e41
Deepak <deepak@central.tech>
2020-08-23 20:30:21 +0700

    error handling

--- src/server.js
+++ src/server.js
@@ -4,35 +4,68 @@ const vision = require("@hapi/vision");
 const { PORT, HOST } = require("./server.config");
 const { configureRoutes } = require("./routesConfig");
 const { configureViews } = require("./viewConfig");
-
-const server = Hapi.Server({
-  port: PORT,
-  host: HOST,
-});
+let server;
 
 /**
- * setup Hapi server, routes, view engine and starts the server.
- * @returns server object representing succesfull server initialization. 
+ * setup server with host and port
+ * @returns server | error object
  */
-const init = async () => {
-  /* Plugin for rendering views as response */
-  await server.register(vision);
-
-  /* configure routes */
-  configureRoutes(server);
-
-  /* configure views */
-  configureViews(server);
-
+const setup = () => {
+  try {
+    server = Hapi.Server({
+      port: "3000",
+      host: HOST,
+    });
+  } catch (e) {
+    console.log(`Server setup: ${e}`);
+    return e;
+  }
   return server;
 }
 
+/**
+ * initialize Hapi server, routes, view engine and starts the server.
+ * @returns server | error object representing succesfull server initialization. 
+ */
+const init = async () => {
+
+  try {
+    // configure server
+    setup();
+
+    if (server) {
+      /* Plugin for rendering views as response */
+      await server.register(vision);
+
+      /* configure routes */
+      configureRoutes(server);
+
+      /* configure views */
+      configureViews(server);
+    }
+  } catch (e) {
+    console.log(`Server init: ${e}`);
+    return e;
+  }
+  return server;
+}
+
+/**
+ * kicks off the server
+ * @returns server | error object
+ */
 const start = async () => {
-  /* kick off the server */
-  await server.start();
-  
-  console.log(`Server running at: ${server.info.uri}`);
-  
+  try {
+    if (server) {
+      /* start the server */
+      await server.start();
+      console.log(`Server running at: ${server.info.uri}`);
+    }
+
+  } catch (e) {
+    console.log(`Server start: ${e}`);
+    return e;
+  }
   return server;
 }
 
